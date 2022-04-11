3c41cd26d477af8be0dfd4d93eef846a09fd4ac0
nilshen <linshen88@gmail.com>
2022-04-11 17:42:46 -0400

    7th

--- Enumerables.rb
+++ Enumerables.rb
@@ -78,23 +78,34 @@ class Array
     end
 
     def my_zip(*arr)
-        h= arr.length
-        w = arr.map(&:length).max
-        new_arr = Array.new(h) {Array.new(w, nil)}
+        arr.unshift(self)
+        h = arr.length
+        maximum = arr.map(&:length).max
+        new_arr = Array.new(maximum) {Array.new(arr.length, nil)}
         (0...h).each do |row|
-            (0...w).each do |col|
-                new_arr[row][col] = arr[]
+            (0...maximum).each do |col|
+                
+                new_arr[row][col] = arr[col][row]
 
+            end
+        end
+        new_arr
     end
 
-
-
-
-
-
-
-
 end
+
+
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
+
+
 
 
 # p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
