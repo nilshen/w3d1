d8cabc594ed7f2d5bad59865ab773f2ecf261b8f
apodavonov3616 <apodavonov3616@DESKTOP-BTS3T0E.localdomain>
2022-04-11 15:20:30 -0400

    first commit

--- Enumerables.rb
+++ Enumerables.rb
@@ -1,7 +1,11 @@
 class Array
 
-    def my_each(&prc)
-        
+    def my_each(array)
+        i = 0
+        while i<array.length
+            puts array[i]
+            i+=1
+        end
     end
 
     
@@ -10,3 +14,20 @@ class Array
 
 
 end
+array = [1,2,3]
+p my_each(array)
+
+# # calls my_each twice on the array, printing all the numbers twice.
+# return_value = [1, 2, 3].my_each do |num|
+#     puts num
+#    end.my_each do |num|
+#     puts num
+#    end
+#    # => 1
+#        2
+#        3
+#        1
+#        2
+#        3
+   
+#    p return_value  # => [1, 2, 3]
